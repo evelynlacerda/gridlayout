eaeafd3032b6414c7e00b2bf330c2f0301fb93b8
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-08-30 00:05:54 -0300

    :sparkles: feat: new pages of grid layout

--- src/js/gridlayout.js
+++ src/js/gridlayout.js
@@ -1,35 +1,39 @@
-const startAgain = document.getElementById('linkHome');
-const docsGrid = document.querySelector('#docs');
-const menu = document.querySelectorAll('#menu button span');
-const buttonGap = document.querySelector('#buttonGap');
-const gapSquare = document.querySelector('#gridSquare');
-
 // BACK TO HOME
+const startAgain = document.querySelector('#linkHome');
+
 startAgain.addEventListener('click', function() {
     var novaURL = "index.html";
     window.location.href = novaURL;
 });
 
-// MENU ACTIVE
-function removeActiveMenu() {
-    menu.forEach((span) => {
-        span.classList.remove('active')
-    })
+// MENU
+const menuLinks = document.getElementsByClassName('button-link');
+const docs = document.querySelector('#docs');
+const grid = document.querySelector('#grid');
+const area = document.querySelector('#area');
+const temp = document.querySelector('#template');
+
+
+
+// FOR MODAL
+const codeButton = document.getElementsByClassName('showDialog');
+const closeModal = document.getElementsByClassName('closemodal');
+const modal = document.getElementsByClassName('modal');
+const backdrop = document.getElementsByClassName('modal-backdrop');
+
+const toggleModal = () => {
+    modal.classList.toggle('hide');
+    backdrop.classList.toggle('hide');
 }
 
-menu.forEach((span) => {
-    span.addEventListener('click', () => {
-        const menuSelect = document.querySelector('.active');
-
-        if (menuSelect) removeActiveMenu();
-
-        span.classList.add('active');
-
-    })
+[codeButton, closeModal, backdrop].forEach((mod) => {
+    mod.addEventListener('click', () => toggleModal());
 });
 
 // GAP FOR GRID
 function gapActive() {
+    const buttonGap = document.querySelector('#buttonGap');
+    const gapSquare = document.querySelector('#gridSquare');
 
     buttonGap.checked
 
