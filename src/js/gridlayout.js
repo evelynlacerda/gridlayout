73fd192300e4ad1883fe1ab53c28751dd11db49d
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-09-01 01:36:04 -0300

    :iphone: Start responsive design

--- src/js/gridlayout.js
+++ src/js/gridlayout.js
@@ -7,49 +7,22 @@ startAgain.addEventListener('click', function () {
 });
 
 // MENU
-const menuLinks = document.querySelectorAll('buttonlink');
+const menuLinks = document.querySelectorAll('.buttonlink');
 const sections = document.querySelectorAll('section');
 
 
 menuLinks.forEach((link) => {
-    link.addEventListener('click', function () {
+    link.addEventListener('click', function() {
 
         const target = link.dataset.target;
         sections.forEach((section) => {
-            console.log(target);
             if (section.getAttribute('id') === target) {
-                section.classList.remove('hideSection');
+                section.style.display = 'flex';
             } else {
-                section.classList.add('hideSection');
+                section.style.display = 'none';
             }
         });
     });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-// FOR MODAL
-const codeButton = document.getElementsByClassName('showDialog');
-const closeModal = document.getElementsByClassName('closemodal');
-const modal = document.getElementsByClassName('modal');
-const backdrop = document.getElementsByClassName('modal-backdrop');
-
-const toggleModal = () => {
-    modal[0].classList.toggle('hide');
-    backdrop[0].classList.toggle('hide');
-}
-
-[codeButton, closeModal, backdrop].forEach((mod) => {
-    mod[0].addEventListener('click', () => toggleModal());
 });
 
 // GAP FOR GRID
